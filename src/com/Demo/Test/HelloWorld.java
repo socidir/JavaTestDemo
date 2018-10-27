c18146b980ad0968199cb4735841476b62468985
socidir <fengtao1127@live.com>
2018-10-27 13:47:26 +0800

    HelloWorld

--- src/com/Demo/Test/HelloWorld.java
+++ src/com/Demo/Test/HelloWorld.java
@@ -3,5 +3,10 @@ package com.Demo.Test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println(add(1,2));
+    }
+    public static int add(int x,int y)
+    {
+        return x+y;
     }
 }
