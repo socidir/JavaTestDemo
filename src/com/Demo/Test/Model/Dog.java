1977da492b7dfeaaadc43081fa629ca51439affa
socidir <fengtao1127@live.com>
2018-10-28 14:57:49 +0800

    添加类修改

--- src/com/Demo/Test/Model/Dog.java
+++ src/com/Demo/Test/Model/Dog.java
@@ -7,11 +7,36 @@ package com.Demo.Test.Model;
 public class Dog {
     private int age;
     private String name;
+
     public Dog() {
 
     }
-    public void  Run()
-    {
-        System.out.println(this.name +" is Run");
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void Run() {
+        System.out.println(this.name + " is Run");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
